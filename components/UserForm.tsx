9e2dfacb7b5a7aeb18e18b28b1712417520bfdf0
Michal Delura <michal.delura@mountainwarehouse.com>
2019-12-31 17:37:39 +0100

    Add form validation

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -3,6 +3,7 @@ import { Button, Content, Form, Text, Toast } from 'native-base';
 import { User } from '../models/user';
 import { database } from '../database/database';
 import { OutlinedTextField } from 'react-native-material-textfield';
+import Joi from 'joi';
 import styles from '../_shared/styles';
 
 export interface UserFormProps {
@@ -11,48 +12,106 @@ export interface UserFormProps {
 }
 
 export interface UserFormState {
-    name: string;
-    email: string;
+    data: {
+        name: string;
+        email: string;
+    };
+    errors: any;
 }
 
 export default class UserForm extends Component<UserFormProps, UserFormState> {
     constructor(props: UserFormProps) {
         super(props);
         this.state = {
-            name: '',
-            email: ''
+            data: {
+                name: '',
+                email: ''
+            },
+            errors: {}
         };
     }
 
+    schema: any = {
+        name: Joi.string()
+            .required()
+            .label('Name'),
+        email: Joi.string()
+            .email({ minDomainAtoms: 2 })
+            .required()
+            .label('Email')
+    };
+
+    handleChange = (name: string, text: string) => {
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(name, text);
+        errors[name] = errorMessage;
+
+        const data: any = { ...this.state.data };
+        data[name] = text;
+        this.setState({ data, errors });
+    };
+
     handleSubmit = async () => {
-        const { name, email } = this.state;
+        const errors = this.validate();
+
+        if (errors) {
+            return this.setState({ errors });
+        }
+        const { name, email } = this.state.data;
         const user = await database.createUser(name, email);
         Toast.show({ text: `Recipient ${name} created!` });
         this.props.onUserCreated(user);
     };
 
-    isValid = (): boolean => {
-        // TODO: Provide validation for the user name and email and unique
-        const { name, email } = this.state;
-        return !(!name || !email);
+    validate = () => {
+        const { error } = Joi.validate(this.state.data, this.schema, { abortEarly: false, stripUnknown: true });
+
+        if (!error) return null;
+
+        const errors: any = {};
+
+        for (const item of error.details) {
+            errors[item.path[0]] = item.message;
+        }
+
+        return errors;
+    };
+
+    validateProperty = (name: string, value: string) => {
+        const obj = { [name]: value };
+        const schema = { [name]: this.schema[name] };
+
+        const { error } = Joi.validate(obj, schema);
+
+        return error ? error.details[0].message : null;
     };
 
     render() {
-        const { name, email } = this.state;
+        const { name, email } = this.state.data;
+        const { errors } = this.state;
+        const isChanged = name || email ? true : false;
+        const isValid = !this.validate();
+        const disabled = !isChanged || !isValid;
 
         return (
             <Content padder>
                 <Form>
-                    <OutlinedTextField label="Name" value={name} onChangeText={text => this.setState({ name: text })} />
+                    <OutlinedTextField
+                        label="Name"
+                        value={name}
+                        onChangeText={text => this.handleChange('name', text)}
+                        error={errors.name}
+                    />
                     <OutlinedTextField
                         label="Email"
                         value={email}
-                        onChangeText={text => this.setState({ email: text })}
+                        onChangeText={text => this.handleChange('email', text)}
+                        error={errors.email}
                     />
                     <Text style={{ fontStyle: 'italic', color: 'grey', fontSize: 14 }}>
                         Can be a distribution list if you want to inform multiple people
                     </Text>
-                    <Button block success disabled={!this.isValid()} onPress={this.handleSubmit} style={styles.button}>
+                    <Button block success disabled={disabled} onPress={this.handleSubmit} style={styles.button}>
                         <Text>Create</Text>
                     </Button>
                 </Form>
