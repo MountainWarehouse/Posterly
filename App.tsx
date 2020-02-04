296e392427da37c439fb27e84f64fa287cee1f53
Michal Delura <michal.delura@mountainwarehouse.com>
2020-02-04 14:39:47 +0100

    Remove unnecessary realm open close

--- App.tsx
+++ App.tsx
@@ -1,34 +1,15 @@
 import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
-import { AppState, AppStateStatus } from 'react-native';
+import React, { useEffect } from 'react';
 import { Root } from 'native-base';
 import { MenuProvider } from 'react-native-popup-menu';
 import realm from './database/Realm';
 import AppNavigationContainer from './navigation/AppNavigationContainer';
 
 const App: React.SFC = () => {
-    const [appState, setAppState] = useState(AppState.currentState);
-
-    //TODO: Figure out if it's necessary and/or if it's not time consuming
-    const handleChangeAppState = (nextAppState: AppStateStatus) => {
-        if (appState !== 'active' && nextAppState === 'active') {
-            realm.open();
-            console.log('app going active');
-        } else if (appState === 'active' && nextAppState !== 'active') {
-            realm.close();
-            console.log(`app going ${nextAppState}`);
-        }
-        setAppState(nextAppState);
-    };
-
     useEffect(() => {
-        console.log('app mount');
         realm.open();
-        AppState.addEventListener('change', handleChangeAppState);
 
         return () => {
-            console.log('app unmount');
-            AppState.removeEventListener('change', handleChangeAppState);
             realm.close();
         };
     }, []);
